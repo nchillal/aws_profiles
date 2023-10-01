81c0642d5a68426e101668b92598008984835984
Nagendra Chillale <>
2023-10-01 12:33:44 -0700

    Added listing of aws regions

--- aws_profiles.go
+++ aws_profiles.go
@@ -1,9 +1,13 @@
 package aws_profiles
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/go-ini/ini"
 )
 
@@ -35,4 +39,33 @@ func ListAWSProfiles() ([]string, error) {
 	}
 
 	return profiles, nil
+}
+
+func ListAWSRegions(awsProfile string) []string {
+	// Load AWS SDK configuration
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithSharedConfigProfile(awsProfile),
+	)
+	if err != nil {
+		fmt.Println("Error loading AWS SDK configuration:", err)
+		return nil
+	}
+
+	// Create an EC2 client
+	ec2Client := ec2.NewFromConfig(cfg)
+
+	// Call DescribeRegions to get a list of regions
+	resp, err := ec2Client.DescribeRegions(context.TODO(), &ec2.DescribeRegionsInput{})
+	if err != nil {
+		fmt.Println("Error describing regions:", err)
+		return nil
+	}
+
+	// Get list of regions
+	regions := make([]string, 0)
+	for _, region := range resp.Regions {
+		regions = append(regions, *region.RegionName)
+	}
+	return regions
 }
